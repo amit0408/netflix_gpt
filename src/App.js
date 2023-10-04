021de7617fd59e25d9d07f85bf9d90f660bd4b58
“Amit <amit.bisht@landmarkgroup.com>
2023-10-05 01:08:08 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="text-lime-600 text-5xl  ">
-      <p>Hello</p>
+    <div className="text-lime-600 text-5xl p-5 ">
+      <p>HELLO NETFLIX WELCOME !!!!</p>
     </div>
   );
 }
