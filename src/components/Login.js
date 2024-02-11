a22bf50f2dcf5067056d3edc67430877c9075152
“Amit <amit.bisht@landmarkgroup.com>
2024-02-11 23:23:07 +0530

    "test"

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,11 +19,11 @@ const Login = () => {
           />  
         </div>
         <form className='absolute w-3/12  p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
-          <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In":"Sign Up"} </h1>
+          <h1 className='font-bold text-3xl py-4'> {isSignInForm ? "Sign In":"Sign Up"} </h1>
           <input type='text' name="email" placeholder='email' className='p-4 my-4 w-full bg-gray-700 ' />
           <input type='text' name="password" placeholder='password' className='p-4 my-4 w-full bg-gray-700 '/>
-          <button className=' p-4 my-6 bg-red-700 w-full rounded-lg' onClick={toggleSignInForm}> Sign In</button>
-          <p>New to Netflix? Sign up now</p>
+          <button className=' p-4 my-6 bg-red-700 w-full rounded-lg' > {isSignInForm ? "Sign In":"Sign Up"}</button>
+          <p onClick={toggleSignInForm}>New to Netflix? Sign up now</p>
         </form>
    </div>
   )
