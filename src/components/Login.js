f1f8428ee5eab6df5c66ad94a4fd0523fa0c42f3
“Amit <amit.bisht@landmarkgroup.com>
2024-02-12 00:09:07 +0530

    "signin_signup_completed"

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,10 +20,11 @@ const Login = () => {
         </div>
         <form className='absolute w-3/12  p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
           <h1 className='font-bold text-3xl py-4'> {isSignInForm ? "Sign In":"Sign Up"} </h1>
+          {!isSignInForm &&<input type='text' name="password" placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700 '/>}
           <input type='text' name="email" placeholder='email' className='p-4 my-4 w-full bg-gray-700 ' />
           <input type='text' name="password" placeholder='password' className='p-4 my-4 w-full bg-gray-700 '/>
           <button className=' p-4 my-6 bg-red-700 w-full rounded-lg' > {isSignInForm ? "Sign In":"Sign Up"}</button>
-          <p onClick={toggleSignInForm}>New to Netflix? Sign up now</p>
+          <p  className='cursor-pointer'onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix ? Sign Up now" : "Already user ? Sign In Now"}</p>
         </form>
    </div>
   )
